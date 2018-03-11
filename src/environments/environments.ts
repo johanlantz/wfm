1bbb533967ef3b2515bc6131e9c3fa194c0a880e
Johan Lantz <johan.lantz@telefonica.com>
2018-03-11 23:06:44 +0100

    remove fb file

--- src/environments/environments.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    firebase : {
-        apiKey: "AIzaSyAJpDPhaPg9MgWrK_0HOa2F1vZ1HIbAr3Y",
-        authDomain: "ionic-cloud-f08b6.firebaseapp.com",
-        projectId: "ionic-cloud-f08b6"
-    }
- };
